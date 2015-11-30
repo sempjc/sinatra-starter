0ffa63484958b1fba25d477cfbc6f3b52e81e143
sempjc <jc.semprit@hotmail.com>
2015-11-30 16:35:49 -0400

    Initial Commit and creation of erb files in the view folder, representing the diferent seccion of the questionary

--- src/routes/default.rb
+++ src/routes/default.rb
@@ -1,8 +1,24 @@
 class MyApplication < Sinatra::Base
 
   #Routes
-  get "/" do
-    "HELLO WORLD!!!!"
+  get "/1" do
+    erb :section_1
+  end
+
+  get "/2" do
+    erb :section_2
+  end
+
+  get "/3" do
+    erb :section_3
+  end
+  
+  get "/4" do
+    erb :section_4
+  end
+
+  get "/5" do
+    erb :section_5
   end
 
 end
